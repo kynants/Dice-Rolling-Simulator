7978eb0215fd49a4fd25f4b635f6d0e4aeae4d23
Kynan Stevenson <kynants@gmail.com>
2019-01-15 16:16:13 -0500

    Redesigned code to be object-oriented. Not finished yet.

--- program.py
+++ program.py
@@ -4,15 +4,17 @@
 import time     # Used for pausing the program
 import random   # Used for generating random numbers
 
-# Prints out a message
-print('Rolling the dice...')
-
-# Pause for 2 seconds while the dice rolls.
-time.sleep(2)
-
 # "Constants"
 MIN_NUM = 1
 MAX_NUM = 6
 
-for x in range(MIN_NUM, MAX_NUM):
-    print(random.randint(MIN_NUM, MAX_NUM))
+class dice:
+    def __init__(self):
+        # Prints out a message
+        print('Rolling the dice...')
+
+        # Pause for 2 seconds while the dice rolls.
+        time.sleep(2)
+
+        # for x in range(MIN_NUM, MAX_NUM):
+        #     print(random.randint(MIN_NUM, MAX_NUM))
